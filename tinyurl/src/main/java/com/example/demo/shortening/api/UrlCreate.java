2541273e9683edee142caf3977f707eca5dd7700
Oresztesz Margaritisz <oresztesz_margaritisz@epam.com>
2021-03-22 11:19:04 +0100

    Added host-port configuration

--- tinyurl/src/main/java/com/example/demo/shortening/api/UrlCreate.java
+++ tinyurl/src/main/java/com/example/demo/shortening/api/UrlCreate.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import com.example.demo.shortening.api.dto.UrlEntity;
 import com.example.demo.shortening.api.dto.UrlShortened;
 import com.example.demo.shortening.domain.UrlShortener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -14,9 +15,12 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/url")
 public class UrlCreate {
 
+    @Autowired
+    private UrlShortener shortener;
+
     @PostMapping
     Mono<ResponseEntity<UrlShortened>> create(@RequestParam String apiKey, @RequestBody UrlEntity url) throws MalformedURLException {
-        var shortened = UrlShortener.shorten(new URL(url.originalUrl));
+        var shortened = shortener.shorten(new URL(url.originalUrl));
         return Mono.just(ResponseEntity
                 .ok().header("apiKey", apiKey)
                 .body(new UrlShortened(url.originalUrl, shortened.toString())));
--- tinyurl/src/test/java/com/example/demo/shortening/domain/UrlShortenerTest.java
+++ tinyurl/src/test/java/com/example/demo/shortening/domain/UrlShortenerTest.java
@@ -1,18 +1,28 @@
-package com.example.demo.shortening;
+package com.example.demo.shortening.domain;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import com.example.demo.shortening.domain.UrlShortener;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UrlShortenerTest {
 
+    private UrlShortener urlShortener;
+
+    @BeforeEach
+    public void setUp() {
+        urlShortener = new UrlShortener();
+        urlShortener.setServerAddress("localhost");
+        urlShortener.setServerPort("8080");
+    }
+
     @Test
     public void testResolve() throws MalformedURLException {
         String target = "https://google.com/xasd";
-        var result = UrlShortener.shorten(new URL(target));
+        var result = urlShortener.shorten(new URL(target));
         Assertions.assertEquals(7, result.getPath().length());
     }
 
@@ -20,8 +30,8 @@ class UrlShortenerTest {
     public void testResolveWithDifferentEnding() throws MalformedURLException {
         String target = "https://google.com/xasd";
         String target2 = "https://google.com/ImANewEnding";
-        var result = UrlShortener.shorten(new URL(target));
-        var result2 = UrlShortener.shorten(new URL(target2));
+        var result = urlShortener.shorten(new URL(target));
+        var result2 = urlShortener.shorten(new URL(target2));
         Assertions.assertNotEquals(result, result2);
     }
 
