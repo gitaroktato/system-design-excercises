078b133fd1b68c614d57caec84dbf06b21f1f2c3
Oresztesz Margaritisz <oresztesz_margaritisz@epam.com>
2021-03-23 11:19:13 +0100

    Added more detailed docs and benchmark:

--- tinyurl/src/jmh/java/com.example.tinyurl.shortening.domain/UrlShortenerBenchmark.java
+++ tinyurl/src/jmh/java/com.example.tinyurl.shortening.domain/UrlShortenerBenchmark.java
@@ -6,16 +6,31 @@ import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 public class UrlShortenerBenchmark {
 
     @Benchmark
     public URL benchmarkShorten() throws MalformedURLException {
         var shortener = new UrlShortener();
+        shortener.setServerAddress("localhost");
+        shortener.setServerPort("8080");
         var target = "https://google.com/";
         byte[] array = new byte[7]; // length is bounded by 7
         new Random().nextBytes(array);
         var generatedString = new String(array, StandardCharsets.UTF_8);
         return shortener.shorten(new URL(target + generatedString));
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(UrlShortenerBenchmark.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
     }
 }
