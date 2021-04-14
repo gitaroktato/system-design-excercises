7013cb533c81ec73352dca1924d76f57d20e34bb
Oresztesz Margaritisz <oresztesz_margaritisz@epam.com>
2021-04-14 11:53:21 +0200

    Added API docs with WebFlux:

--- tinyurl/resolver/src/main/java/com/example/tinyurl/resolving/api/UrlResolvingRouter.java
+++ tinyurl/resolver/src/main/java/com/example/tinyurl/resolving/api/UrlResolvingRouter.java
@@ -1,8 +1,10 @@
 package com.example.tinyurl.resolving.api;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springdoc.core.fn.builders.apiresponse.Builder.responseBuilder;
+import static org.springdoc.core.fn.builders.parameter.Builder.parameterBuilder;
+import static org.springdoc.webflux.core.fn.SpringdocRouteBuilder.route;
 
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -13,7 +15,13 @@ public class UrlResolvingRouter {
 
     @Bean
     public RouterFunction<ServerResponse> urlResolve(UrlResolvingHandler handler) {
-        return route(GET("/{hash}"), handler::resolve);
+        return route().GET("/{hash}", handler::resolve, ops -> ops
+                .operationId("resolve")
+                .summary("Resolve URL by the given shortened hash")
+                .parameter(parameterBuilder().in(ParameterIn.PATH).name("hash").description("Shortened URL hash"))
+                .response(responseBuilder().responseCode("308").description("Redirection to the resolved URL"))
+                .response(responseBuilder().responseCode("400").description("Malformed URL during resolution"))
+        ).build();
     }
 
 }
