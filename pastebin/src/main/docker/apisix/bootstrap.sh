f9a1d5581d9fe67195e749fa96a4362270100295
Oresztesz Margaritisz <oresztesz_margaritisz@epam.com>
2021-11-17 16:12:14 +0100

    Added automation for APISIX

--- pastebin/src/main/docker/apisix/bootstrap.sh
+++ pastebin/src/main/docker/apisix/bootstrap.sh
@@ -1,1 +1,14 @@
-curl -d@/work/routes.json http://apisix:9080/apisix/admin/routes/1 -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT
+#!/bin/sh
+
+do_with_retry()
+{
+  timeout 5m sh -c "until ${1}; do sleep 10; done"
+}
+
+submit_json()
+{
+  do_with_retry "curl -d@${1} http://apisix:9080/apisix/admin/routes/1 -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT"
+}
+
+submit_json /work/user-routes.json
+submit_json /work/paste-routes.json
