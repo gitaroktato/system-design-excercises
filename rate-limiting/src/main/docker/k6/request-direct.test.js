2c3e81448557603110c20aa6b1f648577861d8c2
Oresztesz Margaritisz <oresztesz_margaritisz@epam.com>
2023-02-17 13:31:43 +0100

    Added distribution of response times and direct tests

--- rate-limiting/src/main/docker/k6/request-direct.test.js
+++ rate-limiting/src/main/docker/k6/request-direct.test.js
@@ -1,5 +1,12 @@
 import http from 'k6/http';
+import { check } from 'k6';
 
 export default function () {
-  http.get('http://localhost:8080/direct/key?id=key1');
+  for (let id = 1; id <= 3; id++) {
+    const res = http.get(`http://localhost:8080/direct/key?id=key${id}&apiKey=api_one`);
+    check(res, {
+      'is status 200': (r) => r.status === 200,
+      'body is correct': (r) => r.body == `value${id}`
+    });
+  }
 }
