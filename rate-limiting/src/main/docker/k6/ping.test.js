6f66598a57fff6a34c14987b09ec0c7f89654848
Oresztesz Margaritisz <oresztesz_margaritisz@epam.com>
2023-05-10 16:52:49 +0200

    Done with LB config

--- rate-limiting/src/main/docker/k6/ping.test.js
+++ rate-limiting/src/main/docker/k6/ping.test.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('http://localhost:8080/ping');
+  http.get('http://localhost/ping');
   // sleep(1);
 }
